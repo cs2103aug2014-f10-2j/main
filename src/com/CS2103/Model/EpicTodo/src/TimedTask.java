b1241ca94c84644d681f71b3620da038ed5f646a
a0111683 <a0111683@A0111683.stu.nus.edu.sg>
2014-10-02 06:06:20 +0800

    Created base classes

---
+++ src/com/CS2103/Model/EpicTodo/src/TimedTask.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+
+class TimedTask extends BaseClass {
+	public static Date insertStartDate(Date calendar) {
+		Date result = new Date();
+		return result;
+	}
+	
+	public static Date insertEndDate(Date calendar) {
+		Date result = new Date();
+		return result;
+	}
+}
