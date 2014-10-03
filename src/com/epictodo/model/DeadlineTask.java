e4451c34820a80264e2b58b09191bba1078765b6
Eric <liulongyin@gmail.com>
2014-10-03 18:18:22 +0800

    Revert "Revert "V0.1: [Naming] Renamed some packages""
    This reverts commit 74ab3eeedce5ccc9b27cb6dbf77e57354e74b369.

---
+++ src/com/epictodo/model/DeadlineTask.java
@@ -0,0 +1,10 @@
+package com.epictodo.model;
+
+import java.util.Date;
+
+class DeadlineTask extends BaseClass {
+	public static Date insertDateline(Date calendar) {
+		Date result = new Date();
+		return result;
+	}
+}
---
+++ src/com/epictodo/model/FloatingTask.java
@@ -0,0 +1,10 @@
+package com.epictodo.model;
+
+import java.util.Date;
+
+class FloatingTask extends BaseClass {
+	public static Date createReminder(Date calendar) {
+		Date result = new Date();
+		return result;
+	}
+}
