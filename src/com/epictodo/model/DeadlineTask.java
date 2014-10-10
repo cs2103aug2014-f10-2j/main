bbbfd5cf38bc20ee9d5c1126032e3656752e09d9
Eric <liulongyin@gmail.com>
2014-10-10 08:30:07 +0800

    [Update] Model classes

--- src/com/epictodo/model/DeadlineTask.java
+++ src/com/epictodo/model/DeadlineTask.java
@@ -1,76 +1,35 @@
-//package com.epictodo.model;
+package com.epictodo.model;
 
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 
-class DeadlineTask extends Task {
+public class DeadlineTask extends Task {
 	/************** Data members **********************/
-	private static ArrayList<String> Tasks = new ArrayList<String>();
-	private String taskName;
-	private String taskDescription;
-	private String date;
-	private String time;
-	private String dateTime;
-	private long unixTimeStamp;
+	private long endDateTime;
 
 	/************** Constructors **********************/
-	DeadlineTask(String taskName, String ddmmyy, String time, String desc) {
-		setTaskName(taskName);
-		setDate(ddmmyy);
-		setTime(time);
-		setTaskDescription(taskDescription);
-		try{
-		setDateTime(ddmmyy, time);
-		}catch(Exception e){
-			
-		}
+	public DeadlineTask(String taskName, String taskDescription, int priority, long endDateTime) {
+		super(taskName, taskDescription, priority);
+		this.endDateTime = endDateTime;
 	}
 	
 	/**************** Accessors ***********************/
-	String getTaskName() {
-		return taskName;
-	}
-	
-	String getDate() {
-		return date;
-	}
-	
-	String getTime() {
-		return time;
-	}
-
-	String getTaskDescription() {
-		return taskDescription;
-	}
-	//This method converts the stored unixTimeStamp into "ddMMyy HH:mm"
-	String getDateTime() {
-		dateTime = new java.text.SimpleDateFormat("ddMMyy HH:mm").format(new java.util.Date (unixTimeStamp*1000));
-		return dateTime;
+	public long getEndDateTime(){
+		return endDateTime;
 	}
 
 	/**************** Mutators ************************/
-	void setTaskName(String newTask) {
-		taskName = newTask;
+	void setEndDateTime(long endDateTime) {
+		this.endDateTime = endDateTime;
 	}
 	
-	void setDate(String newDate) {
-		date = newDate;
-	}
-	
-	void setTime(String newTime) {
-		time = newTime;
-	}
-
-	void setTaskDescription(String newTaskDescription) {
-		taskDescription = newTaskDescription;
-	}
-	
-	void setDateTime(String date, String time) throws ParseException {
+/*	void setDateTime(String date, String time) throws ParseException {
 		String dateTimeTemp = date + " " + time;
 		long epoch = new java.text.SimpleDateFormat("ddMMyy HH:mm").parse(dateTimeTemp).getTime() / 1000;
 		unixTimeStamp = epoch;
 	}
+*/	
 	
 /*	
 	public static Date insertStartDate(Date calendar) {
--- src/com/epictodo/model/FloatingTask.java
+++ src/com/epictodo/model/FloatingTask.java
@@ -1,14 +1,10 @@
 package com.epictodo.model;
 
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Date;
-
-class FloatingTask extends Task {
+public class FloatingTask extends Task {
 	/************** Data members **********************/
 
 	/************** Constructors **********************/
-	FloatingTask(String taskName, String taskDescription, int priority,
+	public FloatingTask(String taskName, String taskDescription, int priority,
 			boolean isDone) {
 		super(taskName, taskDescription, priority);
 	}
