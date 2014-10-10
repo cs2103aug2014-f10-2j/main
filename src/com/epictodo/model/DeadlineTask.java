149b29f218a7747a3cad49ba1e0827e6f0c2cfb7
Yeo Kangwei <yeokangwei@hotmail.com>
2014-10-10 08:40:29 +0800

    Updated DeadlineTask.java

--- src/com/epictodo/model/DeadlineTask.java
+++ src/com/epictodo/model/DeadlineTask.java
@@ -9,19 +9,26 @@ public class DeadlineTask extends Task {
 	private long endDateTime;
 
 	/************** Constructors **********************/
-	public DeadlineTask(String taskName, String taskDescription, int priority, long endDateTime) {
+	public DeadlineTask(String taskName, String taskDescription, int priority, String ddmmyy, String time) {
 		super(taskName, taskDescription, priority);
-		this.endDateTime = endDateTime;
+		try {
+			setDateTime(ddmmyy, time);
+		}
+		catch(Exception e){}
 	}
 	
 	/**************** Accessors ***********************/
-	public long getEndDateTime(){
-		return endDateTime;
+	public String getEndDateTime() {
+		String dateTime = new java.text.SimpleDateFormat("ddMMyy HH:mm").format(new java.util.Date (endDateTime*1000));
+		return dateTime;
 	}
 
 	/**************** Mutators ************************/
-	void setEndDateTime(long endDateTime) {
-		this.endDateTime = endDateTime;
+	public void setDateTime(String date, String time) throws ParseException {
+		String dateTimeTemp = date + " " + time;
+		//long epoch = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").parse("01/01/1970 01:00:00").getTime() / 1000;
+		long epoch = new java.text.SimpleDateFormat("ddMMyy HH:mm").parse(dateTimeTemp).getTime() / 1000;
+		endDateTime = epoch;
 	}
 	
 /*	void setDateTime(String date, String time) throws ParseException {
