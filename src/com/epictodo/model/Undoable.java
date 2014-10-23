4bc0e32d86389051a6b31d8b748f7a33b1185ea0
Eric <liulongyin@gmail.com>
2014-10-24 04:23:45 +0800

    V0.2 [updated] chnaged Undoable.java from interface to class

--- src/com/epictodo/model/Undoable.java
+++ src/com/epictodo/model/Undoable.java
@@ -7,6 +7,28 @@ package com.epictodo.model;
  * @author Eric
  *
  */
-public interface Undoable {
-	public void undo();
+public class Undoable {
+	enum CommandType {
+		ADD, DELETE, UPDATE
+	}
+
+	private CommandType _type;
+	private Task _target;
+	private Task _replacement;
+
+	public Undoable(CommandType type, Task target) {
+		_type = type;
+		_target = target;
+		_replacement = null;
+	}
+
+	public Undoable(CommandType type, Task target, Task replacement) {
+		_type = type;
+		_target = target;
+		_replacement = replacement;
+	}
+
+	public void Undo() {
+		// TODO: implement undo code for different types of Undoable
+	}
 }
