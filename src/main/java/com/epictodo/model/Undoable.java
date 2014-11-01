481143fcdefac5055ee9d99948361f0666026a94
Eric Liu <liulongyin@gmail.com>
2014-11-02 04:29:11 +0800

    [V0.4] Updated Undoable.java to support undo() in CRUDLogic.java

--- src/main/java/com/epictodo/model/Undoable.java
+++ src/main/java/com/epictodo/model/Undoable.java
@@ -3,32 +3,81 @@
  */
 package com.epictodo.model;
 
+import java.util.ArrayList;
+
 /**
  * @author Eric
  *
  */
 public class Undoable {
-	enum CommandType {
+	public enum CommandType {
 		ADD, DELETE, UPDATE
 	}
 
+	private ArrayList<Task> _container;
 	private CommandType _type;
 	private Task _target;
 	private Task _replacement;
 
-	public Undoable(CommandType type, Task target) {
+	public Undoable(ArrayList<Task> container, CommandType type, Task target) {
+		_container = container;
 		_type = type;
 		_target = target;
 		_replacement = null;
 	}
 
-	public Undoable(CommandType type, Task target, Task replacement) {
+	public Undoable(ArrayList<Task> container, CommandType type, Task target,
+			Task replacement) {
+		_container = container;
 		_type = type;
 		_target = target;
 		_replacement = replacement;
 	}
 
-	public void Undo() {
-		// TODO: implement undo code for different types of Undoable
+	public String undo() {
+		String result = "";
+
+		switch (_type) {
+		case ADD:
+			_container.remove(_target);
+			result = "adding task \"" + _target.getTaskName() + "\" is undone";
+			break;
+
+		case DELETE:
+			_container.add(_target);
+			result = "deleting task \"" + _target.getTaskName()
+					+ "\" is undone";
+			break;
+
+		case UPDATE:
+			Task temp = _replacement;
+			_replacement = _target;
+			_target = temp;
+			result = "updating task \"" + _target.getTaskName()
+					+ "\" is undone";
+			break;
+		}
+
+		return result;
+	}
+
+	public String toString() {
+		String desc = "command: ";
+		switch (_type) {
+		case ADD:
+			desc += "add ";
+			break;
+		case DELETE:
+			desc += "delete ";
+			break;
+		case UPDATE:
+			desc += "update ";
+			break;
+		}
+		desc += "\"" + _target.toString() + "\"";
+		if (_type == CommandType.UPDATE) {
+			desc += " to \"" + _replacement.toString() + " \"";
+		}
+		return desc;
 	}
 }
