81aa53a77e902f38da3f25e90306793c043ffb2b
Yeo Kangwei <yeokangwei@hotmail.com>
2014-11-01 21:51:23 +0800

    [added] Utility.java under package together with Main.java

---
+++ src/main/java/com/epictodo/Utility.java
@@ -0,0 +1,30 @@
+package com.epictodo;
+
+public class Utility {
+	/******** Global Variables for Storage.java******/
+	public static String FILE_ASSERTED = "Filename: \'storage.txt\' has been asserted.";
+	public static String ARRAYLIST_EMPTY = "ArrayList<Task> is empty.";
+	public static String STORAGE_SUCCESSFUL = "Successfully stored JSON results to Storage.";
+	
+	
+	/******** Global Variables for StorageException.java*******/
+	public static String ERROR_MESSAGE = "UNKNOWN_EXCEPTION";
+	
+	
+	/******** Global Variables for CommandWorker.java**********/
+	public static String INVALID_COMMAND = "Invalid command!";
+	public static String FT_CREATE = "Floating task is created!";
+	public static String DL_ENDTIME = "DeadLine task with default endtime is created!";
+	public static String TT_CREATE = "Timed task is created!";
+	public static String DL_CREATE = "Deadline task is created!";
+	
+	
+	/******** Global Variables for CRUDLogic.java***************/
+	public static String NULL_POINTER_EXCEPTION = "Keyword must not be <null>.";
+	public static String ILLEGAL_ARGUMENT_EXCEPTION = "Illegal priority.";
+	public static String INVALID_INPUT = "Invalid input.";
+	public static String FILE_IO_ERROR = "Failed to create task due to File IO error.";
+	public static String TASK_REMOVED = "Task removed.";
+	public static String REMOVE_TASK_ERROR = "Can't remove task.";
+	public static String TASK_UPDATED = "Task updated";
+}
