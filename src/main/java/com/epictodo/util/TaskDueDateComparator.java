5fdd9209dcbfbf92f44e7279298bc714d443cd8b
Eric <liulongyin@gmail.com>
2014-11-04 18:27:40 +0800

    [V0.4] Added TaskDueDateComparator .java

---
+++ src/main/java/com/epictodo/util/TaskDueDateComparator.java
@@ -0,0 +1,34 @@
+package com.epictodo.util;
+
+import java.util.Comparator;
+
+import com.epictodo.model.DeadlineTask;
+import com.epictodo.model.Task;
+import com.epictodo.model.TimedTask;
+
+class TaskDueDateComparator implements Comparator<Task> {
+
+	@Override
+	public int compare(Task o1, Task o2) {
+		long dt1 = 0, dt2 = 0;
+
+		if (o1 instanceof DeadlineTask) {
+			dt1 = ((DeadlineTask) o1).getEndDateTime();
+		} else if (o1 instanceof TimedTask) {
+			dt1 = ((TimedTask) o1).getEndDateTime();
+		}
+		if (o2 instanceof DeadlineTask) {
+			dt2 = ((DeadlineTask) o2).getEndDateTime();
+		} else if (o1 instanceof TimedTask) {
+			dt2 = ((TimedTask) o2).getEndDateTime();
+		}
+
+		if (dt1 > dt2) {
+			return 1;
+		} else if (dt1 < dt2) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+}
