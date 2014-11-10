877f1c7683f10fcee590967aa5f576c1e453a1ae
Eric <liulongyin@gmail.com>
2014-11-10 16:04:22 +0800

    Added comments into Undoable.java interface

--- src/main/java/com/epictodo/model/command/Undoable.java
+++ src/main/java/com/epictodo/model/command/Undoable.java
@@ -1,9 +1,22 @@
 // @author A0112725N
 
+/**
+ * This interface specifies any undoable actions
+ */
 package com.epictodo.model.command;
 
 public interface Undoable {
-    public String undo();
+	/**
+	 * This method enforces the ability to undo an action
+	 * 
+	 * @return the result
+	 */
+	public String undo();
 
-    public String redo();
+	/**
+	 * This method enforces the ability to redo an action
+	 * 
+	 * @return the result
+	 */
+	public String redo();
 }
